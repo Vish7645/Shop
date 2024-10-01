4537cfb125656b508ab86ecc7e87ee50f1e251f2
Vishal Kumar <vk9319035@gmail.com>
2024-10-01 13:35:16 +0530

    LIttle Update

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
+
     <div className='container mx-auto px-4'>
       <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
@@ -138,7 +139,7 @@ const About = () => {
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>First Link
+            </span>First: Link
           </a>
           <a>
             <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
@@ -159,7 +160,7 @@ const About = () => {
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" class="w-3 h-3" viewBox="0 0 24 24">
                 <path d="M20 6L9 17l-5-5"></path>
               </svg>
-            </span>Fourth Link
+            </span>Fourth: Link
           </a>
           <a>
             <span class="bg-indigo-100 text-indigo-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
